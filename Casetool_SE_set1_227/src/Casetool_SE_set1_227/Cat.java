aed8cff0ed495e2f366618022e4cb09dd0103961
Senford Fernandes <senfordfernandes201@gmail.com>
2024-04-29 12:28:20 +0530

    exam

---
+++ Casetool_SE_set1_227/src/Casetool_SE_set1_227/Cat.java
@@ -0,0 +1,17 @@
+package Casetool_SE_set1_227;
+
+public class Cat {
+	private String name;
+	private String color;
+	public Cat(String name, String breed)
+	{
+		this.name= name;
+		this.color=color;
+	}
+	
+	public String getColor()
+	{
+		return color;
+	}
+
+}
